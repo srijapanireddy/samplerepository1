1a54acd6c6ebbbf04ebfbcc825ffef8d6fe4e209
palla <palla@LAPTOP-9J0DLRBC>
2021-11-23 15:22:38 +0530

    code updated

--- src/main/java/com/example/demo/Samplerepository1Application.java
+++ src/main/java/com/example/demo/Samplerepository1Application.java
@@ -8,6 +8,7 @@ public class Samplerepository1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Samplerepository1Application.class, args);
+		System.out.println("i am srija");
 	}
 
 }
