7979b1fb9db68686ff85d7183bd958fdefd78057
palla <palla@LAPTOP-9J0DLRBC>
2021-11-23 15:58:27 +0530

    updated

---
+++ src/main/java/com/example/demo/Samplerepository2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Samplerepository2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Samplerepository2Application.class, args);
+	}
+
+}
